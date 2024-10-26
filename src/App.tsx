52b5d1f75dae89253aa24ca6ce53d0443ec18c8a
Travis Hoki <travis.hoki@gmail.com>
2024-10-26 14:50:15 -0600

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,35 @@
-import React, { ReactNode } from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import './App.css';
-import { Home } from './Home';
-import { Gallery } from './Gallery';
-import { NoPage } from './NoPage';
-
-const Layout = (children:ReactNode) => {
-  return(
-    <div>{children}</div>
-  )
-}
+import React from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./Home";
+import { Gallery } from "./Gallery";
+import { NoPage } from "./NoPage";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <header className="App-header">
-        
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="gallery">Gallery</Link></li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="gallery">Gallery</Link>
+              </li>
+            </ul>
+          </nav>
         </header>
 
         <Routes>
-        <Route path="/" element={<Layout />}>
-        <Route  element={<Home />}>
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="*" element={<NoPage />} />
-        </Route>
-        </Route>
+          <Route index path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="*" element={<NoPage />} />
         </Routes>
-        </BrowserRouter>
+      </Router>
     </div>
   );
 }
-
-
 
 export default App;

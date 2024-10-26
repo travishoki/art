b2d33dcf3849180419b228eb48a15a18344fe7ab
BrandonHill <bchill429@gmail.com>
2024-10-26 15:22:16 -0600

    gallery array

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import "./Gallery.css";
+
+const images:ImageType[] = [
+    {
+        id:"1",
+        title:"one",
+    },{
+        id:"2",
+        title:"two"
+    }
+]
+type ImageType = {
+    id:string,
+    title:string,
+}
 
 export const Gallery = () => {
     return (
+        <div className="gallery">
         <h1>Gallery</h1>
+        <ul>
+        {images.map((image)=>(
+           <li>{image.title}</li>     
+        ))}
+        </ul>
+        </div>
     )
 }
